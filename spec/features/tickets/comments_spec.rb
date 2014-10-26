f0f1e91ab8d8bdc4bc4f4da7f05e442cd6d506c8
Andy <thatandyrose@gmail.com>
2014-10-26 08:37:04 +0000

    added spec for non logged in user adding a comment

--- spec/features/tickets/comments_spec.rb
+++ spec/features/tickets/comments_spec.rb
@@ -4,6 +4,62 @@ feature 'Ticket comments' do
     @ticket = FactoryGirl.create :ticket
   end
 
+  content 'when user is not logged in' do
+
+    before do
+      visit ticket_path(@ticket)
+    end
+
+    context 'and the user adds a comments without any data' do
+      
+      before do
+        click_on 'Add comment'
+        @ticket.reload
+      end
+
+      it 'should validate the lack of content and email' do
+        expect(page).to have_content 'A comment cannot be empty.'
+        expect(page).to have_content 'Your email is required.'
+      end
+
+      it 'should not save any comments' do
+        expect(@ticket.comments.count).to eq 0
+        expect(Comment.count).to eq 0
+      end
+
+    end
+
+    context 'and the user adds a comment with content and email' do
+
+      before do
+        fill_in :text, with: 'some stuff about this ticket'
+        fill_in :email, with: 'whoever@somehwere.com'
+        click_on 'Add comment'
+        @ticket.reload
+      end
+
+      it 'should show the comment on the page' do
+        expect(page).to have_content 'some stuff about this ticket'
+      end
+
+      it 'should add the comment to the ticket' do
+        expect(@ticket.comments.count).to eq 1
+        expect(@ticket.comments.first.text).to eq 'some stuff about this ticket'
+      end
+
+      it 'should add not logged in details to the comment' do
+        expect(@ticket.comments.first.email).to eq 'whoever@somehwere.com'
+        expect(@ticket.comments.first.user).to be_nil
+      end
+
+      it 'should not notify the customer of a new comment' do
+        expect(last_email.to.first).to be_nil
+      end
+
+    end    
+
+  end
+
   context 'when user is logged in' do
     
     before do
