712539072f4157e123c42479462676197ea6c6f3
Andy <thatandyrose@gmail.com>
2014-10-28 19:38:07 +0000

    created spec for activities

---
+++ spec/features/activities/comment_spec.rb
@@ -0,0 +1,81 @@
+feature 'Comment activities' do
+
+  before do
+    @ticket = FactoryGirl.create :ticket
+  end
+
+  context 'when an admin is logged in' do
+
+    before do
+      @user = FactoryGirl.create(:user)
+      signin(@user.email, @user.password)
+
+      visit ticket_path(@ticket)
+    end
+
+    context 'and creates a new comment' do
+
+      before do
+        fill_in :ticket_comments_attributes_0_text, with: 'some stuff about this ticket'
+        click_on 'Add comment'
+      end
+
+      it 'should create a new comment create activity' do
+        expect(Activity.count).to eq 3
+        expect(Activity.where(activity_type: :create).count).to eq 2
+        expect(Activity.where(activity_type: :create).where('comment_id is not null').count).to eq 1
+        
+        expect(Activity.where(activity_type: :create).where(comment_id: Comment.first.id).ticket).to eq @ticket
+        expect(Activity.where(activity_type: :create).where(comment_id: Comment.first.id).comment).to eq Comment.first
+      end
+
+      it 'should create a new status change activity' do
+        expect(Activity.count).to eq 3
+        expect(Activity.where(activity_type: :status_change).count).to eq 1
+        
+        expect(Activity.find_by_activity_type(:status_change).ticket).to eq @ticket
+        
+        expect(page).to have_content 'Test User changed the ticket status to Waiting For Customer Response'
+      end
+
+    end
+  end
+
+  context 'when an no one is logged in' do
+
+    before do
+      visit ticket_path(@ticket)
+    end
+
+    context 'and creates a new comment' do
+
+      before do
+        @ticket.update_attributes! statue_key: 'on_hold'
+        Activity.destroy_all
+
+        fill_in :ticket_comments_attributes_0_text, with: 'some stuff about this ticket'
+        fill_in :ticket_comments_attributes_0_email, with: 'someemail@fortheticket.com'
+        click_on 'Add comment'
+      end
+
+      it 'should create a new comment create activity' do
+        expect(Activity.count).to eq 2
+        expect(Activity.where(activity_type: :create).count).to eq 1
+        
+        expect(Activity.where(comment_id: Comment.first.id).ticket).to eq @ticket
+        expect(Activity.where(comment_id: Comment.first.id).comment).to eq Comment.first
+      end
+
+      it 'should create a new status change activity' do
+        expect(Activity.count).to eq 2
+        expect(Activity.where(activity_type: :status_change).count).to eq 1
+        
+        expect(Activity.find_by_activity_type(:status_change).ticket).to eq @ticket
+        
+        expect(page).to have_content 'someone changed the ticket status to Waiting For Staff Response'
+      end
+
+    end
+  end
+
+end
---
+++ spec/features/activities/ticket_spec.rb
@@ -0,0 +1,137 @@
+feature 'Ticket activities' do
+
+  before do
+    @ticket = FactoryGirl.create :ticket
+  end
+
+  context 'when a ticket is created' do
+
+    it 'should create a create activity for the ticket' do
+      expect(Activity.count).to eq 1
+      expect(Activity.first.activity_type).to eq 'create'
+      expect(Activity.first.ticket).to eq @ticket
+    end
+
+    it 'should show the activity on the page' do
+      visit ticket_path(@ticket)
+
+      expect(page).to have_content 'someone created a new ticket'
+    end
+
+  end
+
+  context 'when an admin is logged in' do
+
+    before do
+      @user = FactoryGirl.create(:user)
+      signin(@user.email, @user.password)
+
+      visit ticket_path(@ticket)
+    end
+
+    context 'and takes ownership of a ticket' do
+      
+      before do
+        click_on 'Own!'
+      end
+
+      it 'should create a own actvity for the ticket' do
+        expect(Activity.count).to eq 2
+        expect(Activity.last.activity_type).to eq 'own'
+        expect(Activity.first.ticket).to eq @ticket
+      end
+
+      it 'should show the activity on the page' do
+        expect(page).to have_content 'Test User took ownership of the ticket'
+      end
+
+    end
+
+    context 'and changes the status of a ticket' do
+
+      before do
+        select 'On Hold', from: :ticket_status_key
+        click_on 'Update status'
+      end
+
+      it 'should create a status change actvity for the ticket' do
+        expect(Activity.count).to eq 2
+        expect(Activity.last.activity_type).to eq 'status_change'
+        expect(Activity.first.ticket).to eq @ticket
+      end
+
+      it 'should show the activity on the page' do
+        expect(page).to have_content 'Test User changed the ticket status to On Hold'
+      end      
+
+    end
+
+    context 'and changes the status of a ticket and closes ticket' do
+
+      before do
+        select 'Cancelled', from: :ticket_status_key
+        click_on 'Update status'
+
+        @ticket.reload
+      end
+
+      it 'should create a status change actvity for the ticket' do
+        expect(Activity.count).to eq 3
+        expect(Activity.where(activity_type: :status_change).count).to eq 1
+        
+        expect(Activity.find_by_activity_type(:status_change).ticket).to eq @ticket
+        expect(Activity.find_by_activity_type(:status_change).user_name).to 'Test User'
+      end
+
+      it 'should create a close activity for the ticket' do
+        expect(Activity.count).to eq 3
+        expect(Activity.where(activity_type: :close).count).to eq 1
+        
+        expect(Activity.find_by_activity_type(:close).ticket).to eq @ticket
+        expect(Activity.find_by_activity_type(:close).user_name).to 'Test User'
+      end
+
+      it 'should show the activities on the page' do
+        expect(page).to have_content 'Test User changed the ticket status to Cancelled'
+        expect(page).to have_content 'Test User closed the ticket'
+      end      
+
+    end
+
+    context 'and changes the status of a closed ticket and opens the ticket' do
+
+      before do
+        @ticket.update_attributes! status_key: :cancelled
+        Activity.destroy_all
+
+        select 'Cancelled', from: :ticket_status_key
+        click_on 'Update status'
+
+        @ticket.reload
+      end
+
+      it 'should create a status change actvity for the ticket' do
+        expect(Activity.count).to eq 3
+        expect(Activity.where(activity_type: :status_change).count).to eq 1
+        
+        expect(Activity.find_by_activity_type(:status_change).ticket).to eq @ticket
+        expect(Activity.find_by_activity_type(:status_change).user_name).to 'Test User'
+      end
+
+      it 'should create a close activity for the ticket' do
+        expect(Activity.count).to eq 3
+        expect(Activity.where(activity_type: :close).count).to eq 1
+        
+        expect(Activity.find_by_activity_type(:close).ticket).to eq @ticket
+        expect(Activity.find_by_activity_type(:close).user_name).to 'Test User'
+      end
+
+      it 'should show the activities on the page' do
+        expect(page).to have_content 'Test User changed the ticket status to Cancelled'
+        expect(page).to have_content 'Test User closed the ticket'
+      end      
+
+    end
+  end
+  
+end
